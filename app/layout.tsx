77dbea314bd2e32eb280ab22e4b2f5d0dfad9333
andrewsabastian531 <chandan60683@gmail.com>
2025-09-07 13:14:20 +0530

    Add favicon and update metadata for AI Interview Saas

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,24 @@
-import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import { Analytics } from '@vercel/analytics/next'
-import './globals.css'
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import { Analytics } from "@vercel/analytics/next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
-}
+  title: "Ai Interviewer",
+  description: "Created with React,TypeScript,Next.js",
+  generator: "Adarsh Sharma",
+  icons: {
+    icon: "favicon.ico",
+    shortcut: "favicon-32x32.png",
+  },
+};
+
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
@@ -22,5 +27,5 @@ export default function RootLayout({
         <Analytics />
       </body>
     </html>
-  )
+  );
 }
